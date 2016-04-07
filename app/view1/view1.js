3e8bf34fced1028a3d999335c469ea597857e356
Scheele <axel.scheele@capgemini.com>
2016-04-07 18:06:59 +0200

    added ideas for updating

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,21 +13,36 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 
-.controller('View1Ctrl', ['$scope', function($scope) {
+.controller('View1Ctrl', ['$scope', '$timeout', function($scope, $timeout) {
     
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Scores', 'Contesters'];
+    $scope.series = ['AverageScores', 'AmountOfTestTakers'];
     $scope.data = [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
         ];
     
-    $scope.doughnutLabels = ["January", "February", "March"];
-    $scope.doughnutSeries = ['Scores'];
+    $scope.doughnutLabels = ["1", "2", "3", "4", "5", "6", "7"];
+    $scope.doughnutSeries = ['Questions'];
     $scope.doughnutData = [
-        65, 59, 80
+        65, 59, 80, 60, 40, 50, 10
     
         ];
-  
+        
+ /*       function dataCtrl($scope, $timeout, Data) {
+    $scope.data = [];
+
+    (function tick() {
+        $scope.data = Data.query(function(){
+            $timeout(tick, 1000);
+        });
+    })();
+    
+};
+ // Updates should be as follows:
+$scope.data.push(value)
+$scope.lables.push(label)
+$scope.doughnutData = newArray
+  */
   
 }]);
